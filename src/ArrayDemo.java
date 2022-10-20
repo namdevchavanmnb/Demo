9dddf9b2ed56093f199ff5eae89d37362089815e
namdevchavanmnb@gmail.com <namdevchavanmnb@gmail.com>
2022-10-20 11:06:48 +0530

    Modified Demo

--- src/ArrayDemo.java
+++ src/ArrayDemo.java
@@ -1,16 +1,18 @@
 import java.util.Scanner;
 
 public class ArrayDemo {
-	
 	public static void main(String[] args) {
+		int a=9;
 		Scanner sc=new Scanner(System.in);
 		System.out.println("Enter the Length of Array");
+		System.out.println(a);
+
 		int length = sc.nextInt();
 		int []array = new int [length];
 		System.out.println("Enter the Parametr");
 		for (int i=0; i<length;i++) {
 		}
-		
+
 	}
 
 }
--- src/com/bridgelabz/Animal.java
+++ src/com/bridgelabz/Animal.java
@@ -1,5 +1,13 @@
 package com.bridgelabz;
 
-public class Animal {
-
+class Animal {
+	void eat() {
+		System.out.println("i am eating ");
+	}
+}
+class dog extends Animal{
+	public static void main(String[] args) {
+		dog obj1 = new dog ();
+		obj1.eat();
+	}
 }
