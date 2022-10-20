426b498f3a769942a1d35db71865aef63d7d1f4d
namdevchavanmnb@gmail.com <namdevchavanmnb@gmail.com>
2022-10-20 10:59:12 +0530

    Welcom

---
+++ src/ArrayDemo.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class ArrayDemo {
+	
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the Length of Array");
+		int length = sc.nextInt();
+		int []array = new int [length];
+		System.out.println("Enter the Parametr");
+		for (int i=0; i<length;i++) {
+		}
+		
+	}
+
+}
